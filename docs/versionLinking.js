e1d32808e36e76157baddbd8f8f78c24511d844a
Wendall Cada <wendallc@83864.com>
2020-11-09 07:15:11 -0800

    1.2.2 rebuild with Node v12.18.3

--- docs/versionLinking.js
+++ docs/versionLinking.js
@@ -1,4 +1,4 @@
-var versionsArray = ["1.2.2"]; // prepend this with the latest version
+var versionsArray = ["1.2.2", "1.2.1", "1.1.1", "1.0.1", "1.0.0", "0.2.1", "0.2.0"]; // prepend this with the latest version
 var observer;
 
 $(document).ready(function () {
